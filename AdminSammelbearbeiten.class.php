2599d37ae504e138b9b04d9a51ca5172bee8d569
Rasmus Fuhse <fuhse@data-quest.de>
2016-05-11 14:13:24 +0200

    dialog in 3.5

--- AdminSammelbearbeiten.class.php
+++ AdminSammelbearbeiten.class.php
@@ -8,7 +8,12 @@ class AdminSammelbearbeiten extends StudIPPlugin implements SystemPlugin, AdminC
     }
 
     public function useMultimode() {
-        return _("Bearbeiten");
+        if (version_compare($GLOBALS['SOFTWARE_VERSION'], "3.4.99", ">=")) {
+            //Damit es im Dialog geöffnet wird
+            return \Studip\Button::createAccept(_("Bearbeiten"), "edit", array('data-dialog' => 1));
+        } else {
+            return _("Bearbeiten");
+        }
     }
 
     public function getAdminCourseActionTemplate($course_id, $values = null, $semester = null) {
--- views/action/checkbox.php
+++ views/action/checkbox.php
@@ -1,12 +1,14 @@
 <? if ($GLOBALS['perm']->have_studip_perm("tutor", $course_id)) : ?>
-    <button type="submit"
-            style="border: none; background: none; cursor: pointer;"
-            data-dialog
-            name="atleast"
-            value="<?= $course_id ?>"
-            title="<?= _("Ausgewählte Veranstaltungen bearbeiten") ?>">
-        <?= Assets::img("icons/20/blue/edit", array('class' => "text-bottom")) ?>
-    </button>
+    <? if (version_compare($GLOBALS['SOFTWARE_VERSION'], "3.4.99", "<")) : ?>
+        <button type="submit"
+                style="border: none; background: none; cursor: pointer;"
+                data-dialog
+                name="atleast"
+                value="<?= $course_id ?>"
+                title="<?= _("Ausgewählte Veranstaltungen bearbeiten") ?>">
+            <?= Assets::img("icons/20/blue/edit", array('class' => "text-bottom")) ?>
+        </button>
+    <? endif ?>
     <input type="checkbox" name="sem_ids[]" value="<?= htmlReady($course_id) ?>" class="text-top">
 
 <? endif ?>
