b30694aa079a3b7ff3added560a27fb65b4ed53c
Rasmus Fuhse <fuhse@data-quest.de>
2016-12-01 12:33:07 +0100

    fix 4.0

--- views/action/checkbox.php
+++ views/action/checkbox.php
@@ -1,14 +1,15 @@
 <? if ($GLOBALS['perm']->have_studip_perm("tutor", $course_id)) : ?>
-    <? if (version_compare($GLOBALS['SOFTWARE_VERSION'], "3.4.99", "<")) : ?>
-        <button type="submit"
-                style="border: none; background: none; cursor: pointer;"
-                data-dialog
-                name="atleast"
-                value="<?= $course_id ?>"
-                title="<?= _("Ausgewählte Veranstaltungen bearbeiten") ?>">
+    <button type="submit"
+            style="border: none; background: none; cursor: pointer;"
+            data-dialog
+            name="atleast"
+            value="<?= $course_id ?>"
+            title="<?= _("Ausgewählte Veranstaltungen bearbeiten") ?>">
+        <? if (version_compare($GLOBALS['SOFTWARE_VERSION'], "3.5.99", "<")) : ?>
             <?= Assets::img("icons/20/blue/edit", array('class' => "text-bottom")) ?>
-        </button>
-    <? endif ?>
+        <? else : ?>
+            <?= Icon::create("edit", "clickable")->asImg(20, array('class' => "text-bottom")) ?>
+        <? endif ?>
+    </button>
     <input type="checkbox" name="sem_ids[]" value="<?= htmlReady($course_id) ?>" class="text-top">
-
 <? endif ?>
